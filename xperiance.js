858f02c4f9de49d3e21cf7d0247d978fcaa91988
Kaïphexu <kaiphexu@outlook.fr>
2022-04-12 14:56:01 +0200

    requête xp

---
+++ xperiance.js
@@ -0,0 +1,30 @@
+const https = require('https');
+const {EventEmitter, once} = require('events');
+const { guild_id } = require('./config.json');
+class responseEmitter extends EventEmitter {}
+
+const options = {
+  hostname: 'mee6.xyz',
+  port: 443,
+  path: '/api/plugins/levels/leaderboard/' + guild_id,
+  method: 'GET'
+};
+
+
+async function xp() {
+	let donnees = "";
+	let response = null;
+	const resEmitter = new responseEmitter();
+	const req = https.request(options,(res)=>{
+		resEmitter.emit('response',res);
+	});
+	req.end();
+	res = await once(resEmitter, 'response');
+	res[0].on('data',(chunk)=>{
+		donnees += chunk;
+	});
+	await once(res[0],'end');
+	return JSON.parse(donnees);
+}
+
+xp();
